320bb937eecf61b75790c9707e12a7b29fed9ec5
Seiji Munetoh <seiji.munetoh@gmail.com>
2012-05-15 13:32:19 +0900

    added testdata

---
+++ spec/rails/abstraction/sample/app/controllers/tags_controller.rb
@@ -0,0 +1,86 @@
+class TagsController < ApplicationController
+  before_filter :authenticate_user!
+
+  # GET /tags
+  # GET /tags.xml
+  def index
+    @tags = current_user.tags.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @tags }
+    end
+  end
+
+  # GET /tags/1
+  # GET /tags/1.xml
+  def show
+    @tag = current_user.tags.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @tag }
+    end
+  end
+
+  # GET /tags/new
+  # GET /tags/new.xml
+  def new
+    @tag = current_user.tags.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @tag }
+    end
+  end
+
+  # GET /tags/1/edit
+  def edit
+    @tag = current_user.tags.find(params[:id])
+  end
+
+  # POST /tags
+  # POST /tags.xml
+  def create
+    @tag = current_user.tags.new(params[:tag])
+    @tag.user_id = current_user.id
+
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to(@tag, :notice => 'Tag was successfully created.') }
+        format.xml  { render :xml => @tag, :status => :created, :location => @tag }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @tag.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tags/1
+  # PUT /tags/1.xml
+  def update
+    @tag = current_user.tags.find(params[:id])
+
+    respond_to do |format|
+      if @tag.update_attributes(params[:tag])
+        format.html { redirect_to(@tag, :notice => 'Tag was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @tag.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tags/1
+  # DELETE /tags/1.xml
+  def destroy
+    @tag = current_user.tags.find(params[:id])
+    @tag.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(tags_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
